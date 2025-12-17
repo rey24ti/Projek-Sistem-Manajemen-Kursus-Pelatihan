3b3f231c883f199db2157d2219ab31a75316b6c5
firdausfikriii <firdaus24ti@mahasiswa.pcr.ac.id>
2025-12-17 21:31:15 +0700

    Progres2

---
+++ storage/framework/views/8f828664e2ff81b1ae218aac3d3c3f9d.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('guest'); ?>
+    <?php if(\Request::is('login/forgot-password')): ?> 
+        <?php echo $__env->make('layouts.navbars.guest.nav', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+        <?php echo $__env->yieldContent('content'); ?> 
+    <?php else: ?>
+        <div class="container position-sticky z-index-sticky top-0">
+            <div class="row">
+                <div class="col-12">
+                    <?php echo $__env->make('layouts.navbars.guest.nav', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+                </div>
+            </div>
+        </div>
+        <?php echo $__env->yieldContent('content'); ?>        
+        <?php echo $__env->make('layouts.footers.guest.footer', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+    <?php endif; ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\BPF_PIDOO\laragon-6.0-minimal\www\Projek-Sistem-Manajemen-Kursus-Pelatihan\resources\views/layouts/user_type/guest.blade.php ENDPATH**/ ?>
